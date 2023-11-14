02067e68260acd4d0c7620817affe728821ddf8a
Daniel V. <71486704+danvitu@users.noreply.github.com>
2023-11-14 19:45:23 +0300

    Add choosing grid size. Change background color.

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
-const container = document.querySelector('.container');
-
-const addRow = () => {
-  const content = document.createElement('div');
-  content.classList.add('row');
-  container.appendChild(content);
-}
-
-for (let i = 0; i < 16; i++) {
-  addRow();
-}
-
-const rows = document.querySelectorAll('.row');
-
-rows.forEach((element) => {
-  for (let i = 0; i < 16; i++) {
-    const newcell = document.createElement('div');
-    newcell.classList.add('cell');
-    element.appendChild(newcell);
+// Create rows
+function addRows (gridSize) {
+  const container = document.querySelector('.container');
+  for (let i = 0; i < gridSize; i++) {
+    const content = document.createElement('div');
+    content.classList.add('row');
+    container.appendChild(content);
   };
+};
+
+// Create cells
+function addCells (gridSize) {
+  const rows = document.querySelectorAll('.row');
+  rows.forEach((element) => {
+    for (let i = 0; i < gridSize; i++) {
+      const newcell = document.createElement('div');
+      newcell.classList.add('cell');
+      element.appendChild(newcell);
+    };
+  });
+};
+
+const btn = document.querySelector('.button');
+btn.addEventListener('click', () => {
+  clearGrid();
+  let gridSize = prompt('Enter grid size < 100');
+  if (gridSize > 100) {
+    alert('Enter value < 100!');
+    gridSize = prompt('Enter grid size < 100');
+  };
+  createGrid(gridSize);
 });
 
-const cells = document.querySelectorAll('.cell');
+// Create grid
+function createGrid (gridSize) {
+  addRows(gridSize);
+  addCells(gridSize);
+  hovering();
+};
 
+// Clear grid
+function clearGrid () {
+  const container = document.querySelector('.container');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  };
+}
+
+// Create default 16x16 grid
+createGrid(16);
+
+// Add hovering effect
+function hovering () {
+const cells = document.querySelectorAll('.cell');
 cells.forEach((element) => {
-  element.addEventListener('click', () => {
-    element.classList.toggle('hover');
+  element.addEventListener('mouseover', () => {
+    element.classList.add('hover');
   });
 });
+};
